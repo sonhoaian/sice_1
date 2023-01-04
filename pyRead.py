6b58b61f5c4382d53f1aca8fae898feaa9714972
sonhoaian <36180719+sonhoaian@users.noreply.github.com>
2023-01-04 12:37:14 +0700

    Update pyRead.py

--- pyRead.py
+++ pyRead.py
@@ -1,12 +1,8 @@
 import time
 import os
-import random
+import random, string
 import webbrowser, platform
 
-file = open("text.txt", "r")
-#os.system("cmd.exe taskkill /F /IM chrome.exe")
-strg = str(file.read())
-file.close()
 #chrome_exe = "chrome.exe "
 default_text = "checkine kite"
 google_search = ["https://www.google.com/search?q=", 
@@ -15,10 +11,7 @@ google_search = ["https://www.google.com/search?q=",
 
 #strg = a.split()
 #print(type(strg))
-strg_len = 251
-#random seed
-
-
+length = 8
 platform = platform.platform()
 
 if ("linux" in platform) | ("Linux" in platform):
@@ -29,18 +22,19 @@ else:
     taskkill = "taskkill /F /IM  chrome.exe"
 
 os.system(taskkill)
+
+def get_random_string(length):
+    # choose from all lowercase letter
+    letters = string.ascii_letters
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    result_str = result_str +" "+''.join(random.choice(string.digits) for i in range(length))
+    return result_str
+
 counta = 0
-for i in range(1,strg_len):
+for i in range(1,100000000):
     if counta%20 == 0:        
-        os.system(taskkill)
-    seed = (int) (time.time() % strg_len)
-    while seed-1 == 0 | seed - 1 == -1:
-        time.sleep(time.time() % 11)	
-        seed = (int) (time.time() % strg_len)
-    print(seed)
-    rand =  random.randint(0, seed-1)
-    print(rand)
-    google_URL = google_search[i%5] + strg[0:rand] + " " +default_text
+        os.system(taskkill)    
+    google_URL = google_search[i%5] + str(get_random_string(length))
     print(google_URL);
     webbrowser.register(chrome_exe,
 	None,
@@ -48,4 +42,13 @@ for i in range(1,strg_len):
     webbrowser.get(chrome_exe).open(google_URL)    
     time.sleep(45)
     counta+=1
+    
 os.system(taskkill)
+
+###########
+
+
+
+
+
+
